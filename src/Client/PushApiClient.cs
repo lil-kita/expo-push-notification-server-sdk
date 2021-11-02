f165de54ae3f34375188cf01cd8514cbabfd4d8e
Mikita Slaunikau <SlavnikovNik@gmail.com>
2021-11-02 12:25:02 +0300

    rename methods

--- src/Client/PushApiClient.cs
+++ src/Client/PushApiClient.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Expo.Server.Client
@@ -12,8 +11,8 @@ namespace Expo.Server.Client
     public class PushApiClient
     {
         private const string _expoBackendHost = "https://exp.host";
-        private const string _pushSendPath = "/--/api/v2/push/send";
-        private const string _pushGetReceiptsPath = "/--/api/v2/push/getReceipts";
+        private const string _sendPushPath = "/--/api/v2/push/send";
+        private const string _getReceiptsPath = "/--/api/v2/push/getReceipts";
 
         private readonly HttpClientHandler _httpHandler;
         private readonly HttpClient _httpClient;
@@ -30,21 +29,21 @@ namespace Expo.Server.Client
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
 
-        public async Task<PushTicketResponse> PushSendAsync(List<PushTicketRequest> pushTicketRequest) // It may either be a single message object or a list of up to 100 message objects
+        public async Task<PushTicketResponse> SendPushAsync(List<PushTicketRequest> pushTicketRequest) // It may either be a single message object or a list of up to 100 message objects
         {
             StringContent requestBody = Serialize(pushTicketRequest);
-            PushTicketResponse ticketResponse = await SendRequest<PushTicketResponse>(_pushSendPath, requestBody);
+            PushTicketResponse ticketResponse = await PostAsync<PushTicketResponse>(_sendPushPath, requestBody);
             return ticketResponse;
         }
 
-        public async Task<PushResceiptResponse> PushGetReceiptsAsync(PushReceiptRequest pushReceiptRequest) // Make sure you are only sending a list of 1000 (or less) ticket ID strings
+        public async Task<PushResceiptResponse> GetReceiptsAsync(PushReceiptRequest pushReceiptRequest) // Make sure you are only sending a list of 1000 (or less) ticket ID strings
         {
             StringContent requestBody = Serialize(pushReceiptRequest);
-            PushResceiptResponse receiptResponse = await SendRequest<PushResceiptResponse>(_pushGetReceiptsPath, requestBody);
+            PushResceiptResponse receiptResponse = await PostAsync<PushResceiptResponse>(_getReceiptsPath, requestBody);
             return receiptResponse;
         }
 
-        private StringContent Serialize<T>(T obj) where T: class
+        private StringContent Serialize<T>(T obj) where T : class
         {
             string serializedRequestObj = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
             {
@@ -53,17 +52,15 @@ namespace Expo.Server.Client
             return new StringContent(serializedRequestObj, System.Text.Encoding.UTF8, "application/json");
         }
 
-        private async Task<U> SendRequest<U>(string path, StringContent requestBody)
+        private async Task<U> PostAsync<U>(string path, StringContent requestBody)
         {
             U responseBody = default;
             HttpResponseMessage response = await _httpClient.PostAsync(path, requestBody);
-
             if (response.IsSuccessStatusCode)
             {
                 string rawResponseBody = await response.Content.ReadAsStringAsync();
                 responseBody = JsonConvert.DeserializeObject<U>(rawResponseBody);
             }
-
             return responseBody;
         }
     }
