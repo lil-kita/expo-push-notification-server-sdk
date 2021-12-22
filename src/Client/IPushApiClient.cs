e41a9395a666d414d9a965527a9a44c9c3b80ff6
Mikita Slaunikau <SlavnikovNik@gmail.com>
2021-12-22 12:01:54 +0300

    add interface

---
+++ src/Client/IPushApiClient.cs
@@ -0,0 +1,12 @@
+﻿using ExpoCommunityNotificationServer.Models;
+using System.Threading.Tasks;
+
+namespace ExpoCommunityNotificationServer.Client
+{
+    public interface IPushApiClient
+    {
+        Task<PushTicketResponse> SendPushAsync(params PushTicketRequest[] pushTicketRequest);
+
+        Task<PushResceiptResponse> GetReceiptsAsync(PushReceiptRequest pushReceiptRequest);
+    }
+}
