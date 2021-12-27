3c45a19ef54048b0cd47b64e722b5bd94694087b
Mikita Slaunikau <SlavnikovNik@gmail.com>
2021-12-27 19:28:05 +0300

    add http exception

---
+++ src/Exceptions/HttpPostException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace ExpoCommunityNotificationServer.Exceptions
+{
+    internal class HttpPostException : HttpRequestException
+    {
+        private readonly HttpStatusCode _status;
+
+        public HttpPostException(HttpStatusCode status) : base()
+        {
+            _status = status;
+        }
+
+        public new HttpStatusCode StatusCode() => _status;
+    }
+}
