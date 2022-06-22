e83df7f2af2a172bc356e3001e5ebf7be29c76fa
Mikita Slaunikau <SlavnikovNik@gmail.com>
2022-06-22 19:25:20 +0400

    fix

--- src/Exceptions/HttpPostException.cs
+++ src/Exceptions/HttpPostException.cs
@@ -10,7 +10,19 @@ namespace ExpoCommunityNotificationServer.Exceptions
     [Serializable()]
     public class HttpPostException : HttpRequestException
     {
+#if NET6_0_OR_GREATER
         public HttpPostException(Exception ex, HttpStatusCode? status = null) 
             : base("Exception on request", ex, status) { }
+
+#else
+        private readonly HttpStatusCode? _status;
+
+        public HttpPostException(Exception ex, HttpStatusCode? status = null): base("Exception on request", ex) 
+        {
+            _status = status;
+        }
+
+        public HttpStatusCode? StatusCode => _status;
+#endif
     }
 }
